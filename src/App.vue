3f4090952c6e24757b32582558fe68ae08618bf6
vokyo <machenggong0610@gmail.com>
2026-04-04 01:59:51 +1300

    creating Navbar and Card components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
+import HomeCard from "@/components/HomeCard.vue";
 </script>
 
 <template>
   <Navbar />
   <Hero title="Welcome to Task Tracker" subtitle="Manage your tasks" />
+  <HomeCard />
 </template>
