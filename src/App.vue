5e5628ad2aaa10e8aa9f95913605375a82010579
vokyo <machenggong0610@gmail.com>
2026-04-03 17:54:20 +1300

    Navbar and Hero components

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,9 @@
-<script>
-  export default {
-    data(){
-      return{
-        name: 'Micheal',
-        status: 'pending',
-        tasks: ['Task one', 'Task two', 'Task threee'],
-        link: 'https://google.com'
-      };
-    },
-    methods: {
-      toggleStatus(){
-        if(this.status === 'active'){
-          this.status = 'pending';
-        }
-        else if(this.status === 'pending'){
-          this.status = 'inactive';
-        }
-        else{
-          this.status = 'active';
-        }
-      },
-    },
-  };
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
 </script>
 
-
 <template>
-  <h1>{{ name }}</h1>
-  <p v-if="status === 'active'">User is active</p>
-  <p v-else-if="status === 'pending'">User is pending</p>
-  <p v-else>User is inactive</p>
-  <h3>Tasks:</h3>
-  <ul>
-    <li v-for = "task in tasks" :key="task">{{ task }}</li>
-  </ul>
-  <a :href="link">Click for google</a>
-  <br />
-  <button @click="toggleStatus">Change Status</button>
+  <Navbar />
+  <Hero title="Welcome to Task Tracker" subtitle="Manage your tasks" />
 </template>
-
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { Server } from 'node:http'
@@ -8,6 +9,7 @@ import { Server } from 'node:http'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     vue(),
     vueDevTools(),
   ],
